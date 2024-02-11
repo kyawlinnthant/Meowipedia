f28d344206b6026dbbd3d3c7526f75fe6a869b1b
kyawsoewin <kyawsoewin.august@gmail.com>
2024-02-11 21:41:57 +0630

    Implement Testing Case For Fetch Categories

--- features/categories/data/src/main/java/com/everest/categories/data/model/categories/CategoryDTO.kt
+++ features/categories/data/src/main/java/com/everest/categories/data/model/categories/CategoryDTO.kt
@@ -7,62 +7,62 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CategoryDTO(
-    val adaptability: Int?,
+    val adaptability: Int? = -1,
     @SerialName("affection_level")
-    val affectionLevel: Int?,
+    val affectionLevel: Int? = -1,
     @SerialName("alt_names")
-    val altNames: String?,
+    val altNames: String? = "",
 //    @SerialName("cfa_url")
 //    val cfaUrl: String?,
     @SerialName("child_friendly")
-    val childFriendly: Int?,
+    val childFriendly: Int? = -1,
     @SerialName("country_code")
-    val countryCode: String?,
+    val countryCode: String? = "",
     @SerialName("country_codes")
-    val countryCodes: String?,
-    val description: String?,
+    val countryCodes: String? = "",
+    val description: String? = "",
     @SerialName("dog_friendly")
-    val dogFriendly: Int?,
+    val dogFriendly: Int? = -1,
     @SerialName("energy_level")
-    val energyLevel: Int?,
-    val experimental: Int?,
-    val grooming: Int?,
-    val hairless: Int?,
+    val energyLevel: Int? = -1,
+    val experimental: Int? = -1,
+    val grooming: Int? = -1,
+    val hairless: Int? = -1,
     @SerialName("health_issues")
-    val healthIssues: Int?,
-    val hypoallergenic: Int?,
-    val id: String?,
-    val image: CategoryImageDTO,
-    val indoor: Int?,
-    val intelligence: Int?,
-//    val lap: Int?,
+    val healthIssues: Int? = -1,
+    val hypoallergenic: Int? = -1,
+    val id: String? = "",
+    val image: CategoryImageDTO? = null,
+    val indoor: Int? = -1,
+    val intelligence: Int? = -1,
+//    val lap: Int? = -1,
     @SerialName("life_span")
-    val lifeSpan: String?,
-    val name: String?,
-    val natural: Int?,
-    val origin: String?,
-    val rare: Int?,
+    val lifeSpan: String? = "",
+    val name: String? = "",
+    val natural: Int? = -1,
+    val origin: String? = "",
+    val rare: Int? = -1,
     @SerialName("reference_image_id")
-    val referenceImgId: String?,
-    val rex: Int?,
+    val referenceImgId: String? = "",
+    val rex: Int? = -1,
     @SerialName("shedding_level")
-    val sheddingLevel: Int?,
+    val sheddingLevel: Int? = -1,
     @SerialName("short_legs")
-    val shortLegs: Int?,
+    val shortLegs: Int? = -1,
     @SerialName("social_needs")
-    val socialNeeds: Int?,
+    val socialNeeds: Int? = -1,
     @SerialName("stranger_friendly")
-    val strangerFriendly: Int?,
+    val strangerFriendly: Int? = -1,
     @SerialName("suppressed_tail")
-    val suppressedTail: Int?,
-    val temperament: String?,
+    val suppressedTail: Int? = -1,
+    val temperament: String? = "",
 //    @SerialName("vcahospitals_url")
-//    val vcahoSpitalsUrl: String?,
+//    val vcahoSpitalsUrl: String? = "",
 //    @SerialName("vetstreet_url")
-//    val vetStreetUrl: String?,
-    val vocalisation: Int?,
-    val weight: Weight?,
+//    val vetStreetUrl: String? = "",
+    val vocalisation: Int? = -1,
+    val weight: Weight? = null,
 //    @SerialName("wikipedia_url")
-//    val wikipediaUrl: String?
+//    val wikipediaUrl: String? = ""
 )
 
